5b7544a2fd0d9ce6b93b4907852566a668a791fe
Antoine Macia <antoine.macia83@gmail.com>
2020-01-05 10:50:49 +1100

    Create tabs navigator

--- navigation/MealsNavigator.jsx
+++ navigation/MealsNavigator.jsx
@@ -1,9 +1,11 @@
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createAppContainer } from 'react-navigation';
 import MealDetailScreen from '../screens/MealDetailScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import CategoriesScreen from '../screens/CategoriesScreen';
 import Colors from '../constants/Colors'
+import FavoritesScreen from '../screens/FavoritesScreen';
 
 const MealsNavigator = createStackNavigator(
   {
@@ -24,4 +26,14 @@ const MealsNavigator = createStackNavigator(
   }
 )
 
-export default createAppContainer(MealsNavigator)
+const MealsFavTabNavigator = createBottomTabNavigator(
+  {
+    // Use the stack navigator as the Meals tab,
+    // So that it can be nested and only navigator is in use
+    Meals: MealsNavigator,
+    Favorites: FavoritesScreen
+
+  }
+)
+
+export default createAppContainer(MealsFavTabNavigator)
