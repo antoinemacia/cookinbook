4338feece4e5cacdb926e48631e5c18a3f22ea41
Antoine Macia <antoine.macia83@gmail.com>
2020-01-06 08:35:49 +1100

    Add meal detail screen

--- screens/MealDetailScreen.jsx
+++ screens/MealDetailScreen.jsx
@@ -1,20 +1,46 @@
 import React from 'react';
-import { View, StyleSheet, Button, Text } from 'react-native';
+import { ScrollView, View, StyleSheet, Button, Image } from 'react-native';
 import { MEALS } from '../data/meals';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 import CustomHeaderButton from '../components/CustomHeaderButton'
+import BodyText from '../components/BodyText';
 
 const MealDetailScreen = props => {
+  const mealId = props.navigation.getParam('mealId');
+  const meal = MEALS.find(meal => mealId === meal.id)
+
   return (
-    <View style={styles.screen}>
-      <Button
-        title="Go Back to Categories"
-        onPress={
-          // popToTop sends back to the original top of the stack screen
-          () => { props.navigation.popToTop() }
+    <ScrollView>
+      <Image source={{uri: meal.imageUrl}} style={styles.image} />
+      <View style={styles.details}>
+        <BodyText>{meal.duration} minutes</BodyText>
+        <BodyText>{meal.complexity.toUpperCase()}</BodyText>
+        <BodyText>{meal.affordability.toUpperCase()}</BodyText>
+      </View>
+      <BodyText style={styles.title}>Ingredients</BodyText>
+        {
+          meal.ingredients.map(ingredient => {
+            return <BodyText style={styles.listItem} key={ingredient}>{ingredient}</BodyText>
+          })
         }
-      />
-    </View>
+      <BodyText style={styles.title}>Steps</BodyText>
+      <View style={styles.ingredients}>
+        {
+          meal.steps.map(step => {
+            return <BodyText style={styles.listItem} key={step}>{step}</BodyText>
+          })
+        }
+      </View>
+      <View style={styles.screen}>
+        <Button
+          title="Go Back to Categories"
+          onPress={
+            // popToTop sends back to the original top of the stack screen
+            () => { props.navigation.popToTop() }
+          }
+        />
+      </View>
+    </ScrollView>
   )
 }
 
@@ -46,6 +72,27 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  image: {
+    width: '100%',
+    height: 200
+  },
+  details: {
+    flexDirection: 'row',
+    padding: 15,
+    justifyContent: 'space-around'
+  },
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 20,
+    textAlign: 'center'
+  },
+  listItem: {
+    marginVertical: 10,
+    marginHorizontal: 20,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    padding: 10
   }
 });
 
